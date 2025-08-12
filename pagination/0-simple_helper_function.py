83fe53a15675eaed6feba8db66583eaa340b0da3
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-12 10:44:41 +0100

    task0 file

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,4 +6,5 @@ module for basic pagination
 
 def index_range(page, page_size):
     """function that return start and end index"""
-    return {(page_size*(page-1)), page_size*page}
+    lista = ((page_size*(page-1)), page_size*page)
+    return lista
