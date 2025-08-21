36df37bfe5ff97bb09fc027fcde9e2d612d8e1e6
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-21 22:14:13 +0100

    task8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-# Return all documents in a collection
+""" 9-insert_school """
+
+
 def list_all(mongo_collection):
+    """ 9-insert_school """
     if mongo_collection is None:
         return []
     return list(mongo_collection.find())
