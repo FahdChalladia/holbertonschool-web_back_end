bfcd589de525355b4af6efb4068626c021a1ee5c
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-21 22:47:39 +0100

    task 9 & 10 & 11 files

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+
+  for (const value of array) {
+    result.push(appendString + value);
+  }
+
+  return result;
+}
